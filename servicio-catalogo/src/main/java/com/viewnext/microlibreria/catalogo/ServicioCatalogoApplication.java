d14f17c0bef378b0b212df94700271c8c47db10a
gonzalo <gonzalo.moros.ds@gmail.com>
2016-07-16 01:42:33 +0200

    RestController Objeto

--- servicio-catalogo/src/main/java/com/viewnext/microlibreria/catalogo/ServicioCatalogoApplication.java
+++ servicio-catalogo/src/main/java/com/viewnext/microlibreria/catalogo/ServicioCatalogoApplication.java
@@ -1,8 +1,5 @@
 package com.viewnext.microlibreria.catalogo;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +10,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServicioCatalogoApplication {
 
 	@RequestMapping("/")
-	public Map<String, String> saludo(){
-		return Collections.singletonMap("saludo", "¿Como estás?");
+	public Saludo saludo(){
+		return new Saludo("Hola, ¿Qué tal?","servicio-catalogo");
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioCatalogoApplication.class, args);
 	}
+}
+
+class Saludo{
+	private String saludo;
+	private String app;
+	public Saludo(String saludo, String app) {
+		super();
+		this.saludo = saludo;
+		this.app = app;
+	}
+	public String getSaludo() {
+		return saludo;
+	}
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	public String getApp() {
+		return app;
+	}
+	public void setApp(String app) {
+		this.app = app;
+	}
+	
 }
