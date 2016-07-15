8e9d39c605455cec2403da026a6ea17d83132102
gonzalo <gonzalo.moros.ds@gmail.com>
2016-07-16 01:18:57 +0200

    ResController Simple

--- servicio-catalogo/src/main/java/com/viewnext/microlibreria/catalogo/ServicioCatalogoApplication.java
+++ servicio-catalogo/src/main/java/com/viewnext/microlibreria/catalogo/ServicioCatalogoApplication.java
@@ -2,10 +2,17 @@ package com.viewnext.microlibreria.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServicioCatalogoApplication {
 
+	@RequestMapping("/")
+	public String saludo(){
+		return "Hola, que pasa";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioCatalogoApplication.class, args);
 	}
