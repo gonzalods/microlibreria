18d60e0f948c40d8a8dd987e5c760ce00e1d51a3
0003299 <gms@mail>
2016-07-15 14:21:29 +0200

    Arranque servicio-catalogo con Tomcat

---
+++ servicio-catalogo/src/main/java/com/viewnext/microlibreria/catalogo/ServicioCatalogoApplication.java
@@ -0,0 +1,12 @@
+package com.viewnext.microlibreria.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCatalogoApplication.class, args);
+	}
+}
---
+++ servicio-catalogo/src/test/java/com/viewnext/micro/catalogo/ServicioCatalogoApplicationTests.java
@@ -0,0 +1,20 @@
+package com.viewnext.micro.catalogo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.viewnext.microlibreria.catalogo.ServicioCatalogoApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ServicioCatalogoApplication.class)
+@WebAppConfiguration
+public class ServicioCatalogoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
